60f7c7a381bab6afe65edea6ccf4c8b8fb649071
ultimomauricio <ericuzumakipimentas@gmail.com>
2025-04-24 00:20:34 -0300

    Update Usuario.java

--- src/main/java/com/etec/horacio/PPMariaEduarda/entity/Usuario.java
+++ src/main/java/com/etec/horacio/PPMariaEduarda/entity/Usuario.java
@@ -3,7 +3,6 @@ package com.etec.horacio.PPMariaEduarda.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.Date;
 
 @Entity
 @Table(name = "usuario")
@@ -11,22 +10,33 @@ import java.util.Date;
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idUsuario")
     private int idUsuario;
 
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "NumCelular")
     private int numCelular;
 
+    @Column(name = "nome")
     private String nome;
 
+    @Column(name = "senha")
     private String senha;
 
+    @Column(name = "statusUsuario")
     private String statusUsuario;
 
-    private int anexoCelular;
+    @Column(name = "NumeroCelular")
+    private int numeroCelular;
 
-    private Date date;
+    @Column(name = "ddd")
+    private int ddd;
 
+    @Column(name = "isAdmin")
     private boolean isAdmin;
 
-    private String blog;
+    @Column(name = "foto")
+    private String foto;
 }
